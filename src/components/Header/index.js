604a20324a0b61972578397a2313edc1896d3522
Aimeeri <aimeerimamenova@gmail.com>
2023-03-22 16:53:23 +0600

    update name

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,8 +2,19 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div className="container">
+        <div id="header">
+            <div className="container">
+                <div className="header">
+                    <h2>Restaurant</h2>
+                    <nav className="nav">
+                        <a href="#">interior</a>
+                        <a href="#">About Us</a>
+                        <a href="#">Menu</a>
+                        <a href="#">Contacts</a>
+                    </nav>
 
+                </div>
+            </div>
         </div>
     );
 };
